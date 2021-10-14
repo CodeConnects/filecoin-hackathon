4c0a77a773cb9d6a96f14276db6e7c99f1c560a6
Scott <4674926+CodeConnects@users.noreply.github.com>
2021-10-14 15:35:26 -0400

    add initial challenge 4 page

---
+++ pages/challenge-4.jsx
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+import { Container} from 'react-bootstrap'
+import Navbar from '../components/navbar'
+import Footer from '../components/footer'
+
+export default function Challenge4() {
+    return(
+        <Container className="md-container">
+            <Head>
+                <title>Challenge 4 from the Filecoin NFT Hackathon</title>
+                <link rel="icon" href="/favicon-32x32.png" />
+            </Head>
+
+            <Navbar />
+        
+            <h4 className="text-center pb-4">challenge 4 ~ mint an NFT</h4>
+
+            <img className="d-block mx-auto w-75" src="minted-sky-nft.png"/>
+
+            <Footer />
+        </Container>
+    )
+}
+
+
