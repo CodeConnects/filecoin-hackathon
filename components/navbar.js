6ee0e7d2c2658e041dd74a174eb1d8a99a7ed358
Scott <4674926+CodeConnects@users.noreply.github.com>
2021-10-14 15:33:59 -0400

    add Navbar component

---
+++ components/navbar.js
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+export default function Navbar() {
+    return (
+        <nav className="navigation">
+            <Link href="/">Challenges 1 & 2</Link>
+            <Link href="/challenge-3">Challenge 3</Link>
+            <Link href="/challenge-4">Challenge 4</Link>
+        </nav>
+    )
+}
