151b85fb468034931f1482e274b919dbf71ee1f3
Scott <4674926+CodeConnects@users.noreply.github.com>
2021-10-14 15:19:47 -0400

    add Footer component

---
+++ components/footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="cntr-footer mt-5 mb-5">
+            <p>Built with Node 16.10 / React 17.02 / Next 11.1 / Bootstrap 4.4.1 / Ubuntu 20.04</p>
+        </footer>
+    )
+}
