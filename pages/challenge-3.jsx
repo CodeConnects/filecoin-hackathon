8ec66cf5e07fc0572c5d467aa3a17841b1c670e2
oca <codeconnects@gmail.com>
2021-10-15 10:11:25 -0400

    add link to file on github

--- pages/challenge-3.jsx
+++ pages/challenge-3.jsx
@@ -4,8 +4,6 @@ import { NFTE } from '@nfte/react'
 import Navbar from '../components/navbar'
 import Footer from '../components/footer'
 
-console.log('see me')
-
 export default function Challenge_3() {
     return(
         <Container className="md-container">
@@ -16,14 +14,16 @@ export default function Challenge_3() {
 
             <Navbar />
         
-            <h4 className="text-center pb-5">Challenge 3 ~ Embed an NFT</h4>
+            <h4 className="text-center pb-5"><a href="https://medium.com/encode-club/nft-hack-challenges-c6bafddf30f7#660a" target="_blank">Challenge 3 ~ Embed an NFT</a></h4>
+            
             <Row>
                 <Col>
-                    <p>This solution uses the React module "nfte.app"</p>
+                    <p><strong>This solution uses the React module "nfte.app"</strong></p>
                     <p>After installing the nfte module, an Ethereum NFT can be embedded with a one line component, using just the contract address and token ID</p>
                     <p>React code:<br />
                       <code>&lt;NFTE className="border border-dark" contract="0xb932a70a57673d89f4acffbe830e8ed7f75fb9e0" tokenId="17158"/&gt;</code>
                     </p>
+                    <p><a href="https://github.com/CodeConnects/filecoin-hack-beginner-challenges/blob/main/pages/challenge-3.jsx" target="_blank">Github file link (challenge-3.jsx)</a></p>
                 </Col>
                 <Col>
                     <NFTE className="border border-dark" contract="0xb932a70a57673d89f4acffbe830e8ed7f75fb9e0" tokenId="17158"/>
