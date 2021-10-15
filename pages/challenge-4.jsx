e215c918173c4e374f2e4de01869d3c9ba49a7e1
oca <codeconnects@gmail.com>
2021-10-15 11:38:51 -0400

    add challenge description and solution links

--- pages/challenge-4.jsx
+++ pages/challenge-4.jsx
@@ -12,10 +12,22 @@ export default function Challenge_4() {
             </Head>
 
             <Navbar />
-        
-            <h4 className="text-center pb-4">challenge 4 ~ mint an NFT</h4>
 
-            <img className="d-block mx-auto w-75" src="minted-sky-nft.png"/>
+            <h4 className="text-center pb-4"><a href="https://medium.com/encode-club/nft-hack-challenges-c6bafddf30f7#660a" target="_blank">Challenge 4 ~ Mint an NFT</a></h4>
+
+            <p>This solution uses code forked from the <strong>ipfs-shipyard/nft-school-examples</strong> repo to mint a photo NFT on Rinkeby testnet, then store the metadata on IPFS. Node on an Ubuntu terminal was used for the process.</p>
+            
+            <p><a href="https://github.com/CodeConnects/nft-school-examples/commit/57cca72ed7bb2816334cd29d6607d748062554eb" target="_blank">Modified fork files on Github</a></p>
+
+            <p><a href="https://rinkeby.etherscan.io/address/0xfec7183b9d2133c9fc2284b36c96c284df42a15f" target="_blank">Minting contract depoyed to Rinkeby</a></p>
+
+            <p><a href="https://rinkeby.etherscan.io/tx/0x06549b85a9ebec1f4ca8f8493e93f1a0410dbc613b878f241098f76f2f1c14db" target="_blank">Etherscan minting transaction</a></p>
+
+            <p><a href="https://bafyreibqxvrnneiie6iginzkqlywailebr3xzmpexeeunf7qb5vyj7awuu.ipfs.dweb.link/metadata.json" target="_blank">Metadata stored to IPFS through NFT.storage</a></p>
+
+            <p><a href="https://testnets.opensea.io/assets/0xfec7183b9d2133c9fc2284b36c96c284df42a15f/1" target="_blank">NFT on OpenSea<br />
+            <img className="d-block mx-auto w-50 border border-dark" src="minted-sky-nft.png"/>
+            </a></p>
 
             <Footer />
         </Container>
