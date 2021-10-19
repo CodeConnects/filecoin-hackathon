9f228d03aeb9a3e83f89f26358b234d7758aff18
oca <codeconnects@gmail.com>
2021-10-19 11:55:39 -0400

    build page structure out with no content yet

---
+++ pages/random-nft.jsx
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+import { Container } from 'react-bootstrap'
+import Navbar from '../components/navbar'
+import Random_NFT_Viewer from '../components/random_nft_viewer'
+import Footer from '../components/footer'
+
+export default function Random_NFT() {
+  return (
+    <Container className="md-container">
+      <Head>
+        <title>View a randomly generated NFT</title>
+        <link rel="icon" href="/favicon-32x32.png" />
+      </Head>
+      
+      <Navbar />
+
+      <h4 className="text-center pb-5">View a random NFT or paste in a contract and ID</h4>
+
+      <Random_NFT_Viewer />
+
+      <Footer />
+    </Container>
+  )
+}
