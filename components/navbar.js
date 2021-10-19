45723500ad91264f250d8f3d97ea9456b166f05e
oca <codeconnects@gmail.com>
2021-10-19 11:44:32 -0400

    add link to bonus page

--- components/navbar.js
+++ components/navbar.js
@@ -6,6 +6,7 @@ export default function Navbar() {
             <Link href="/">Challenges 1 & 2</Link>
             <Link href="/challenge-3">Challenge 3</Link>
             <Link href="/challenge-4">Challenge 4</Link>
+            <Link href="/bonus">Bonus</Link>
         </nav>
     )
 }
