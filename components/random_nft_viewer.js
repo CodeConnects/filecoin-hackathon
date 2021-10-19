169f025de19539840e8cd54b56fc9ad507f8ac78
oca <codeconnects@gmail.com>
2021-10-19 17:20:02 -0400

    add titles and 4 collections of content

--- components/random_nft_viewer.js
+++ components/random_nft_viewer.js
@@ -5,14 +5,34 @@ import { Button } from 'react-bootstrap'
 export default function Random_NFT_Viewer() {
     
     const collections = [
-        { cont: "0x82262bFba3E25816b4C720F1070A71C7c16A8fc4", start: 1, total: 500 },
-        { cont: "0xa7d8d9ef8D8Ce8992Df33D8b8CF4Aebabd5bD270", start: 159000000, total: 1024 }
+        { cont: "0x82262bFba3E25816b4C720F1070A71C7c16A8fc4", 
+            title: "Solvency by Ezra Miller",
+            start: 1, 
+            total: 500 
+        },
+        { cont: "0xa7d8d9ef8D8Ce8992Df33D8b8CF4Aebabd5bD270", 
+            title: "Fragments of an Infinite Field by Monica Rizzolli",
+            start: 159000000, 
+            total: 1024 
+        },
+        { cont: "0x41A322b28D0fF354040e2CbC676F0320d8c8850d",
+            title: "SuperRare",
+            start: 1,
+            total: 4422
+        },
+        { cont: "0xa7d8d9ef8D8Ce8992Df33D8b8CF4Aebabd5bD270",
+            title: "Alien Insects by Shvembldr",
+            start: 137000000,
+            total: 1000
+        },
     ]
     const numCol = collections.length;
 
     const [collection, setCollection] = useState(Math.floor(Math.random() * numCol));
 
     const [contr, setContr] = useState(collections[collection].cont);
+
+    const [title, setTitle] = useState(collections[collection].title);
     
     const [tid, setTid] = useState(Math.floor(Math.random() * (collections[collection].total) + collections[collection].start));
 
@@ -21,16 +41,20 @@ export default function Random_NFT_Viewer() {
         setCollection(Math.floor(Math.random() * numCol));
         setTid(Math.floor(Math.random() * (collections[collection].total) + collections[collection].start));
         setContr(collections[collection].cont);
+        setTitle(collections[collection].title);
     }
 
     return (
-        <div className="wrap-viewer">
-            <Button className="float-right" onClick={ refresh } variant="success">View New NFT</Button>
-            <p>
-                <strong>contract:</strong> <small>{contr}</small>
-                <br /><strong>token ID:</strong> <small>{tid}</small>
-            </p>
-            <NFTE contract={contr} tokenId={tid} />
-        </div>
+        <>
+            <h5 className="text-center pb-3">A random selection from <span className="nft-title">{title}</span></h5>
+            <div className="wrap-viewer">
+                <Button className="float-right" onClick={ refresh } variant="success">View New NFT</Button>
+                <p>
+                    <strong>contract:</strong> <small>{contr}</small>
+                    <br /><strong>token ID:</strong> <small>{tid}</small>
+                </p>
+                <NFTE contract={contr} tokenId={tid} />
+            </div>
+        </>
     )
 }
