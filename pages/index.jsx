279f06613bb0b25d2b6a5aac6b44dc24141bd10d
oca <codeconnects@gmail.com>
2021-10-21 22:50:46 -0400

    add padding and break text classes

--- pages/index.jsx
+++ pages/index.jsx
@@ -10,29 +10,25 @@ export default function Home() {
         <title>First Two Challenges from the Filecoin NFT Hackathon</title>
         <link rel="icon" href="/favicon-32x32.png" />
       </Head>
-      
+
       <Navbar />
 
-      <h4 className="text-center pb-5"><a href="https://medium.com/encode-club/nft-hack-challenges-c6bafddf30f7#660a" target="_blank">Challenges 1 &  2 ~ NFT.storage and CID</a></h4>
+      <h4 className="text-center pb-4"><a href="https://medium.com/encode-club/nft-hack-challenges-c6bafddf30f7#660a" target="_blank">Challenges 1 &  2 ~ NFT.storage and CID</a></h4>
 
       <p><strong>Get an NFT.storage API token, store a file, then show its CID.</strong></p>
 
-      <p>CID:<br /><a href="https://bafyreih5zxg7wac5ibyi5qhcnqcbn7o6uxn5ilpqqajhuvffpyxv3t5fmy.ipfs.dweb.link/metadata.json" target="_blank">bafyreih5zxg7wac5ibyi5qhcnqcbn7o6uxn5ilpqqajhuvffpyxv3t5fmy</a></p>
-
+      <p className="break-text">CID:<br /><a href="https://bafyreih5zxg7wac5ibyi5qhcnqcbn7o6uxn5ilpqqajhuvffpyxv3t5fmy.ipfs.dweb.link/metadata.json" target="_blank">bafyreih5zxg7wac5ibyi5qhcnqcbn7o6uxn5ilpqqajhuvffpyxv3t5fmy</a></p>
+    
       <p>
-        <pre>
-          <code>JSON:<br />
-            &nbsp;&nbsp;&#123;<br />
-              &nbsp;&nbsp;&nbsp;&nbsp;"name": "blue lines image",<br />
-              &nbsp;&nbsp;&nbsp;&nbsp;"description": "abstract lines with blue tones and soft focus",<br />
-              &nbsp;&nbsp;&nbsp;&nbsp;"image": "<a href='ipfs://bafybeiffzwkp5xypbmzp5bto2miha4peexhkvstqnhi3kyh565b23xqpuu/abstract_lines.jpg' target='_blank'>ipfs://bafybeiffzwkp5xypbmzp5bto2miha4peexhkvstqnhi3kyh565b23xqpuu/abstract_lines.jpg"</a><br />
-              &nbsp;&nbsp;&#125;
-          </code>
-        </pre>
+        <code>JSON:<br />
+          <span className="span1">&#123;</span><br />
+          <span className="span2">"name": "blue lines image",</span><br />
+          <span className="span2">"description": "abstract lines with blue tones and soft focus",</span><br />
+          <span className="span2">"image": "<a href='ipfs://bafybeiffzwkp5xypbmzp5bto2miha4peexhkvstqnhi3kyh565b23xqpuu/abstract_lines.jpg' target='_blank'>ipfs://bafybeiffzwkp5xypbmzp5bto2miha4peexhkvstqnhi3kyh565b23xqpuu/abstract_lines.jpg"</a></span><br />
+          <span className="span1">&#125;</span>
+        </code>
       </p>
-
-      <Footer />
-
+    <Footer />
     </Container>
   )
 }
