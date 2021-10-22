a0349ac2dd0d70a18cdc42f76fc3eb5cdc419a7d
oca <codeconnects@gmail.com>
2021-10-21 21:34:32 -0400

    replace simple nav with responsive nav component

---
+++ components/navigation.js
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+import { Container, Navbar, Nav } from 'react-bootstrap'
+
+export default function Navigation() {
+    return (
+        <div className="navigation">
+            <Navbar bg="dark" variant="dark" expand="sm">
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="/">Challenges 1 & 2</Nav.Link>
+                        <Nav.Link href="/challenge-3">Challenge 3</Nav.Link>
+                        <Nav.Link href="/challenge-4">Challenge 4</Nav.Link>
+                        <Nav.Link href="/random-nft">View Random NFT</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </div>
+    )
+}
